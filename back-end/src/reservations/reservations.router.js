dea56094da98a3882503d3362b439fad76ccc819
rcsmith8 <94479792+rcsmith8@users.noreply.github.com>
2022-12-12 12:08:00 -0600

    Update reservations.router.js

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,12 +1,15 @@
-/**
- * Defines the router for reservation resources.
- *
- * @type {Router}
- */
-
 const router = require("express").Router();
 const controller = require("./reservations.controller");
 
-router.route("/").get(controller.list);
+router.route("/")
+    .get(controller.listReservations)
+    .post(controller.createReservation)
+
+router.route("/:reservation_id/status")
+    .put(controller.updateStatus)
+
+router.route("/:reservation_id")
+    .get(controller.readReservation)
+    .put(controller.updateReservation)
 
 module.exports = router;
