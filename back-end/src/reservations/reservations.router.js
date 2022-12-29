f6128eee736efe78a65589f8d58671fb2165b2e3
rcsmith8 <94479792+rcsmith8@users.noreply.github.com>
2022-12-29 11:28:44 -0600

    Update reservations.router.js

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,15 +1,28 @@
+/**
+ * Defines the router for reservation resources.
+ *
+ * @type {Router}
+ */
+
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/")
-    .get(controller.listReservations)
-    .post(controller.createReservation)
+router
+  .route("/:reservation_id/status")
+  .put(controller.updateReservationStatus)
+  .all(methodNotAllowed);
 
-router.route("/:reservation_id/status")
-    .put(controller.updateStatus)
+router
+  .route("/:reservation_id")
+  .get(controller.read)
+  .put(controller.updateReservation)
+  .all(methodNotAllowed);
 
-router.route("/:reservation_id")
-    .get(controller.readReservation)
-    .put(controller.updateReservation)
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
 
 module.exports = router;
