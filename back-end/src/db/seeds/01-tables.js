c2723e68bb4ff7b5aed601896738d32bac55d6b2
rcsmith8 <94479792+rcsmith8@users.noreply.github.com>
2022-12-30 14:41:15 -0600

    Update 01-tables.js

--- back-end/src/db/seeds/01-tables.js
+++ back-end/src/db/seeds/01-tables.js
@@ -1,6 +1,12 @@
-const seedData = require('./01-tables.json');
-
-exports.seed = async function (knex) {
-  await knex('tables').del();
-  await knex('tables').insert(seedData);
+exports.seed = function (knex) {
+  return knex
+    .raw("TRUNCATE TABLE tables RESTART IDENTITY CASCADE")
+    .then(function () {
+      return knex("tables").insert([
+        { table_name: "Bar #1", capacity: 1 },
+        { table_name: "Bar #2", capacity: 1 },
+        { table_name: "#1", capacity: 6 },
+        { table_name: "#2", capacity: 6 },
+      ]);
+    });
 };
