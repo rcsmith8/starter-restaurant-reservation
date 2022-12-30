70328a57f1bca0d631e105d469cd513b82f81712
rcsmith8 <94479792+rcsmith8@users.noreply.github.com>
2022-12-30 14:56:29 -0600

    Update 2022121202_createTablesTable.js

--- back-end/src/db/migrations/2022121202_createTablesTable.js
+++ back-end/src/db/migrations/2022121202_createTablesTable.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable("tables", (table) => {
-    table.increments("table_id".primary();
+    table.increments("table_id").primary();
     table.string("table_name").notNullable();
      table.integer("capacity").notNullable();
     table.integer("reservation_id").unsigned();
