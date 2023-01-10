6b433efe34fea3c7eb82824c5791379fbf3ecfd0
rcsmith8 <94479792+rcsmith8@users.noreply.github.com>
2023-01-10 12:11:36 -0600

    Update knexfile.js

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -7,13 +7,11 @@
 require('dotenv').config();
 const path = require("path");
 
-const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
-  DEBUG,
-} = process.env;
+const { DATABASE_URL,
+       DATABASE_URL_DEVELOPMENT,
+       DATABASE_URL_TEST,
+       DATABASE_URL_PREVIEW,
+      } = process.env;
 
 module.exports = {
   development: {
