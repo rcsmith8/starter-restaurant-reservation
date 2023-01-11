c71ff14a9762ae08bc020c5babe4c7c633de5e27
rcsmith8 <94479792+rcsmith8@users.noreply.github.com>
2023-01-11 10:14:34 -0600

    Update app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,7 +1,6 @@
+
+if (process.env.USER || process.env.USERNAME) require("dotenv").config();
 const path = require("path");
-
-require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
-
 const express = require("express");
 const cors = require("cors");
 
